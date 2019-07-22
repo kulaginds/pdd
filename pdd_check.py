60c4401d4178b0450bfe4140d3fa3ac812e43e83
Dmitry Kulagin <dkulagin@ozon.ru>
2019-07-22 20:53:47 +0300

    refactoring

--- pdd_check.py
+++ pdd_check.py
@@ -4,21 +4,16 @@ import sys
 
 def main():
 	if len(sys.argv) < 2:
-		print("пример запуска: ./pdd_check.py chapter_11.txt")
+		print("пример запуска:")
+		print(sys.argv[0], "chapter_11.txt")
 		return
 
 	a, count = read_answers_from_file(sys.argv[1])
 
-	while True:
-		print("ответы для проверки:")
+	print("ответы для проверки:")
 
-		b = read_answers_from_stdin(count)
-
-		check_answers(a, b)
-
-		is_retry = input("проверить себя ещё? (y/n): ")
-		if is_retry != "y":
-			return
+	b = read_answers_from_stdin(count)
+	check_answers(a, b)
 
 def read_answers_from_file(file):
 	a = {}
@@ -42,7 +37,7 @@ def read_answers_from_stdin(count):
 def check_answers(a, b):
 	for k in b:
 		if int(a[k]) != int(b[k]):
-			print(str(k) + ": ожидалось " + str(a[k]).strip() + "; выбрано " + str(b[k]).strip())
+			print("вопрос №%d: ожидалось %s; выбрано %s" % (k, str(a[k]).strip(), str(b[k]).strip()))
 
 if __name__ == "__main__":
 	main()
